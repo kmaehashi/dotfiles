31bcd10e5ed83e90e1b7a7e34d911d49e4bc9468
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2014-02-23 23:59:33 +0900

    support cygwin-x64

--- cygwin/setup_apt-cyg.sh
+++ cygwin/setup_apt-cyg.sh
@@ -6,5 +6,5 @@ wget "http://apt-cyg.googlecode.com/svn/trunk/apt-cyg"
 chmod 755 apt-cyg
 mv apt-cyg /bin
 
-apt-cyg --mirror ftp://ftp.jaist.ac.jp/pub/cygwin/ --cache /cygdrive/c/cygwin/cache update
+apt-cyg --mirror ftp://ftp.jaist.ac.jp/pub/cygwin/$(_cygarch) --cache /cygdrive/c/cygwin/cache update
 
