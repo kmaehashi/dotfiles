7233a5019611aef752f2047645517e031252f179
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2017-11-22 11:29:05 +0900

    add pyenv examples

--- utils/pyenv.sh
+++ utils/pyenv.sh
@@ -2,3 +2,7 @@
 
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
 git clone https://github.com/yyuu/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+
+# Examples:
+# env PYTHON_CONFIGURE_OPTS="--enable-shared" pyenv install 3.6.3
+# pyenv virtualenv 3.6.3 local-3.6.3
