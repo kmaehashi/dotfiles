cedf8fb25e8ae85d925ee89718cdb4c210eeeac3
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2018-05-27 23:34:30 +0900

    install hub script

--- utils/hub.sh
+++ utils/hub.sh
@@ -1,2 +1,11 @@
-# TODO
-# install command, zsh completions
+#!/bin/bash -uex
+
+ARCH="linux-amd64"
+VERSION="2.2.9"
+NAME="hub-${ARCH}-${VERSION}"
+
+cd /tmp
+wget "https://github.com/github/hub/releases/download/v${VERSION}/${NAME}.tgz"
+tar xf "${NAME}.tgz" "${NAME}/bin/hub"
+mv "${NAME}/bin/hub" ~/local/bin
+rm -rf "${NAME}"
