7c3e6df954ee6f2463ee0ae61a9797acccf548f3
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2019-11-10 21:32:41 +0900

    use curl

--- utils/hub.sh
+++ utils/hub.sh
@@ -7,7 +7,7 @@ ARCH="$(uname | tr '[:upper:]' '[:lower:]')-amd64"
 NAME="hub-${ARCH}-${VERSION}"
 
 cd /tmp
-wget "https://github.com/github/hub/releases/download/v${VERSION}/${NAME}.tgz"
+curl -Lo "${NAME}.tgz" "https://github.com/github/hub/releases/download/v${VERSION}/${NAME}.tgz"
 tar xf "${NAME}.tgz" "${NAME}/bin/hub"
 mv "${NAME}/bin/hub" ~/local/bin
 rm -rf "${NAME}" "${NAME}.tgz"
