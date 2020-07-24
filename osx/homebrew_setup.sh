0b17ae0478a067494175ee21fcb1f68fcebc91c2
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-07-24 18:36:25 +0900

    update homebrew url

--- osx/homebrew_setup.sh
+++ osx/homebrew_setup.sh
@@ -4,4 +4,4 @@ HOMEBREW_ROOT="/opt/homebrew"
 
 sudo mkdir -p "${HOMEBREW_ROOT}"
 sudo chown "${USER}:" "${HOMEBREW_ROOT}"
-git clone https://github.com/mxcl/homebrew.git "${HOMEBREW_ROOT}"
+git clone https://github.com/Homebrew/brew.git "${HOMEBREW_ROOT}"
