496d34e880879abe2798a1b7648e0fecc6e041db
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2020-08-10 18:11:02 +0900

    add ccache install command

---
+++ utils/ccache.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -uex
+
+# https://github.com/ccache/ccache/releases
+VERSION="3.7.11"
+
+mkdir -p ~/local/src
+pushd ~/local/src
+
+wget "https://github.com/ccache/ccache/releases/download/v${VERSION}/ccache-${VERSION}.tar.gz"
+tar xf "ccache-${VERSION}.tar.gz"
+pushd ccache-${VERSION}
+./configure --prefix="${HOME}/local"
+make
+make install
+
+for C in gcc g++; do
+    cat << _EOF_ > ~/local/bin/ccache-${C}
+#!/bin/sh
+exec ccache ${C} \$@
+_EOF_
+    chmod +x ~/local/bin/ccache-${C}
+done
