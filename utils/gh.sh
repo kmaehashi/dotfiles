7cc95d7688e840a16b1cd75e98b7d67675b08090
Kenichi Maehashi <webmaster@kenichimaehashi.com>
2023-07-01 15:13:38 +0900

    update gh install script

--- utils/gh.sh
+++ utils/gh.sh
@@ -1,21 +1,24 @@
 #!/bin/bash -uex
 
 # https://github.com/cli/cli/releases/
-VERSION="$(curl -s 'https://api.github.com/repos/cli/cli/releases/latest' | grep '"tag_name":' | cut -f4 -d'"' | tr -d v)"
+VERSION="$(curl -f -s 'https://api.github.com/repos/cli/cli/releases/latest' | grep '"tag_name":' | cut -f4 -d'"' | tr -d v)"
 
 if [[ "$(uname)" = "Darwin" ]]; then
     OSNAME="macOS"
+    ARCH="macOS_arm64"
+    EXT="zip"
 else
     OSNAME="$(uname | tr '[:upper:]' '[:lower:]')"
+    ARCH="${OSNAME}_amd64"
+    EXT="tar.gz"
 fi
 
-ARCH="${OSNAME}_amd64"
-#ARCH="${OSNAME}_arm64"
 NAME="gh_${VERSION}_${ARCH}"
 
-
-cd /tmp
-curl -Lo "${NAME}.tgz" "https://github.com/cli/cli/releases/download/v${VERSION}/${NAME}.tar.gz"
-tar xf "${NAME}.tgz" "${NAME}/bin/gh"
+WORKDIR="$(mktemp -d)"
+pushd "${WORKDIR}"
+curl -fL -o "archive" "https://github.com/cli/cli/releases/download/v${VERSION}/${NAME}.${EXT}"
+tar xf "./archive" "${NAME}/bin/gh"
 mv "${NAME}/bin/gh" ~/local/bin
-rm -rf "${NAME}" "${NAME}.tgz"
+popd
+rm -rf "${WORKDIR}"
